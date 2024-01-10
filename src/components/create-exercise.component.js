8d2dcf980b8a7b9ce05d53b680866e16b3e18fb6
Tabhi109 <tabhinav109@gmail.com>
2024-01-10 21:27:29 +0530

    updating routes of the API

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -22,7 +22,7 @@ export default class CreateExercise extends Component {
     }
 
     componentDidMount() {
-        axios.get('http://localhost:5000/users/')
+        axios.get('http://localhost:3000/users/')
           .then(response => {
             if(response.data.length > 0) {
                 this.setState({
@@ -70,7 +70,7 @@ export default class CreateExercise extends Component {
 
         console.log(exercise);
 
-        axios.post('http://localhost:5000/exercises/add', exercise)
+        axios.post('http://localhost:3000/exercises/add', exercise)
           .then(res => console.log(res.data));
 
           window.location = '/';
